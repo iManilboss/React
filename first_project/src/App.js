7816c4777a3c41ea0f93d1e63feb8284a57084cf
iManilboss <imanilboss@gmail.com>
2022-04-25 10:36:04 +0545

    fristcommit

---
+++ first_project/src/App.js
@@ -0,0 +1,13 @@
+import Restaurant from './components/Restaurant';
+import './components/style.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Restaurant/>
+    </div>
+  );
+}
+
+export default App;
